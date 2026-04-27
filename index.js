1a20a5f801c4b2d3f0d6e4974a82b15f1950178c
Niiijima <qagwu11@gmail.com>
2026-04-27 21:07:27 +0100

    database

---
+++ index.js
@@ -0,0 +1,21 @@
+//import express
+const express = require('express');   
+
+//install CORS in order for your backend to communicate with your frontend by typwing npm install cors on your terminal
+
+//cors (cross origin resource sharing)
+const cors = require ('cors');
+
+//create app
+const app = express();
+const port = 7777
+app.use(cors()); //allows any form of resource
+app.use(express.json);
+
+app.get('/', (req, res) => {
+    res.send("api is ready for use");
+});
+
+app.listen(prototype, () => {
+    console.log(`server is running on port ${port}`);
+});
